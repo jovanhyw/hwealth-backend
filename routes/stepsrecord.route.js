cea705580595e30353dda15ca635faecc19b8683
jovanhyw <32458958+jovanhyw@users.noreply.github.com>
2019-10-02 17:33:41 +0800

    Added DEL endpoint for Steps Record

--- routes/stepsrecord.route.js
+++ routes/stepsrecord.route.js
@@ -5,5 +5,6 @@ const stepsRecordService = require('../services/stepsrecord.service');
 router.post('/', stepsRecordService.createStepsRecord);
 router.get('/', stepsRecordService.getAllStepsRecord);
 router.put('/:id', stepsRecordService.updateStepsRecord);
+router.delete('/:id', stepsRecordService.deleteStepsRecord);
 
 module.exports = router;
--- services/stepsrecord.service.js
+++ services/stepsrecord.service.js
@@ -60,7 +60,7 @@ StepsRecordService.updateStepsRecord = async (req, res) => {
   } catch (err) {
     res.status(400).send({
       error: true,
-      message: 'Invalid ID. Failed to retrieve steps record.'
+      message: 'Invalid ID. Failed to update steps record.'
     });
   }
 
@@ -82,7 +82,40 @@ StepsRecordService.updateStepsRecord = async (req, res) => {
   } catch (err) {
     res.status(400).send({
       error: true,
-      message: 'Invalid ID. Failed to retrieve steps record.'
+      message: 'Invalid ID. Failed to update steps record.'
+    });
+  }
+};
+
+StepsRecordService.deleteStepsRecord = async (req, res) => {
+  // input validation
+
+  try {
+    const record = await StepsRecord.findById({ _id: req.params.id });
+
+    if (req.account.accountid !== record.accountId.toString())
+      return res.status(403).send({
+        error: true,
+        message: 'You are not authorized to delete this record.'
+      });
+  } catch (err) {
+    res.status(400).send({
+      error: true,
+      message: 'Invalid ID. Failed to delete steps record.'
+    });
+  }
+
+  try {
+    await StepsRecord.findByIdAndDelete({ _id: req.params.id });
+
+    res.status(200).send({
+      error: false,
+      message: 'Steps Records deleted successfully.'
+    });
+  } catch (err) {
+    res.status(500).send({
+      error: true,
+      message: 'Internal Server Error. Failed to delete steps record.'
     });
   }
 };
