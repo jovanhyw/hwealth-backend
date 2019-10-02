8dce0e7747b32774e7a5e60f10338fd3870c97df
jovanhyw <32458958+jovanhyw@users.noreply.github.com>
2019-10-02 16:42:43 +0800

    Added Steps Record PUT endpoint

--- routes/stepsrecord.route.js
+++ routes/stepsrecord.route.js
@@ -3,5 +3,7 @@ const router = express.Router();
 const stepsRecordService = require('../services/stepsrecord.service');
 
 router.post('/', stepsRecordService.createStepsRecord);
+router.get('/', stepsRecordService.getAllStepsRecord);
+router.put('/:id', stepsRecordService.updateStepsRecord);
 
 module.exports = router;
--- services/stepsrecord.service.js
+++ services/stepsrecord.service.js
@@ -24,4 +24,64 @@ StepsRecordService.createStepsRecord = async (req, res) => {
   }
 };
 
+StepsRecordService.getAllStepsRecord = async (req, res) => {
+  try {
+    const allRecords = await StepsRecord.find(
+      {
+        accountId: req.account.accountid
+      },
+      'dateRecorded totalSteps -_id'
+    ).sort({ dateRecorded: 'descending' });
+
+    res.status(200).send({
+      error: false,
+      message: 'Steps Records retrieved successfully.',
+      records: allRecords
+    });
+  } catch (err) {
+    res.status(400).send({
+      error: true,
+      message: 'Failed to retrieve steps record.'
+    });
+  }
+};
+
+StepsRecordService.updateStepsRecord = async (req, res) => {
+  // input validation
+
+  try {
+    const record = await StepsRecord.findById({ _id: req.params.id });
+
+    if (req.account.accountid !== record.accountId.toString())
+      return res.status(403).send({
+        error: true,
+        message: 'You are not authorized to make a change to this record.'
+      });
+  } catch (err) {
+    res.status(400).send({
+      error: true,
+      message: 'Invalid ID. Failed to retrieve steps record.'
+    });
+  }
+
+  try {
+    const updated = await StepsRecord.findByIdAndUpdate(
+      { _id: req.params.id },
+      req.body,
+      { new: true }
+    );
+
+    res.status(200).send({
+      error: false,
+      message: 'Steps Records updated successfully.',
+      record: updated
+    });
+  } catch (err) {
+    res.status(400).send({
+      error: true,
+      message: 'Invalid ID. Failed to retrieve steps record.'
+    });
+  }
+};
+
 module.exports = StepsRecordService;
