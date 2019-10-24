4eb96561b478a4cb36149177b30fed8fb8a86180
jovanhyw <32458958+jovanhyw@users.noreply.github.com>
2019-10-24 18:51:10 +0800

    Updated Generate Secret service for 2fa

--- services/twofactor.service.js
+++ services/twofactor.service.js
@@ -1,7 +1,27 @@
 const Account = require('../models/Account');
+const bcrypt = require('bcryptjs');
 const speakeasy = require('speakeasy');
 const TwoFactorService = {};
 
-TwoFactorService.generateSecret = async (req, res) => {};
+TwoFactorService.generateSecret = async (req, res) => {
+  // rmb to do input validation to take passwd
+
+  const account = await Account.findById({ _id: req.account.accountid });
+
+  // check if password is correct
+  const validPass = await bcrypt.compare(req.body.password, account.password);
+  if (!validPass)
+    return res.status(401).send({
+      error: true,
+      message: 'Authentication failed.'
+    });
+
+  const secret = speakeasy.generateSecret({ length: 20 });
+
+  res.status(200).send({
+    error: true,
+    secret
+  });
+};
 
 module.exports = TwoFactorService;
