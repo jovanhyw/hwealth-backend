3297f424f534e5210ffe1b996672cfed543ee1ee
Jeremy Tan <fearlessjem17@gmail.com>
2019-11-07 15:30:19 +0800

    changes to profile route and service, added custom end point

--- routes/profile.route.js
+++ routes/profile.route.js
@@ -7,4 +7,7 @@ router.put('/update-profile', profileService.updateProfile);
 router.put('/update-bmi', profileService.calculateBMI);
 router.get('/professionals', profileService.getPro);
 
+//custom endpoint
+router.get('/getChatUsers', profileService.getChatUser);
+
 module.exports = router;
